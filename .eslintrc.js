aafdb5317d8daa6545df4d8f22eec5d6d1973a5e
Alex <aredwood@users.noreply.github.com>
2020-02-15 12:59:33 +1100

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    "env": {},
+    "extends": [
+        "plugin:@typescript-eslint/recommended",
+        "plugin:@typescript-eslint/recommended-requiring-type-checking"
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "project": "tsconfig.json",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "@typescript-eslint",
+    ]
+};
